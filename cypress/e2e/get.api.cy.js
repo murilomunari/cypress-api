89e50d3ceea3bcfc8d2b00de17fdd1af407fb1b8
Murilo <mu_bissiato@hotmail.com>
2025-05-05 22:15:23 -0300

    Usando validações para o body da request

--- cypress/e2e/get.api.cy.js
+++ cypress/e2e/get.api.cy.js
@@ -2,11 +2,21 @@ describe('Buscar dispositivos', () => {
 
     it('Buscar dispositivo especifico', () => {
 
+        const deviceId = '7'; // ID do dispositivo que você deseja buscar
+
         cy.request({ 
             method: 'GET',
-            url: 'https://api.restful-api.dev/objects/7',
+            url: `https://api.restful-api.dev/objects/${deviceId}`, 
             failOnStatusCode: false,
+        }).as('getDeviceResult')
+
+        // validações
+        cy.get('@getDeviceResult').then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.id).to.equal(deviceId);
         });
+
+        
 
     });
 })
