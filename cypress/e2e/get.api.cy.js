31053ba8ff95bcdfa794a4b5e3ab1d1138b12e77
Murilo <mu_bissiato@hotmail.com>
2025-05-05 23:54:42 -0300

    Verificando todos os requisitos do body

--- cypress/e2e/get.api.cy.js
+++ cypress/e2e/get.api.cy.js
@@ -14,6 +14,20 @@ describe('Buscar dispositivos', () => {
         cy.get('@getDeviceResult').then((response) => {
             expect(response.status).to.equal(200);
             expect(response.body.id).to.equal(deviceId);
+            expect(response.body.name).to.equal('Apple MacBook Pro 16');
+
+
+            expect(response.body).not.to.be.empty;
+            expect(response.body.data).to.exist.and.to.not.be.empty;
+
+            expect(response.body.data.year).not.string;
+            expect(response.body.data.year).to.equal(2019);
+
+
+            expect(response.body.data.price).not.string;
+            expect(response.body.data.year).to.equal(1849.99);
+
+            expect(response.body.data['CPU model']).to.exist.and.to.not.be.empty;
         });
 
         
